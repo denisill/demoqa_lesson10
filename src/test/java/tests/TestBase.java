da155219413a53aa944087e87038491424cd44e7
Denis_illarionov <denisill90@yandex.ru>
2021-12-05 00:36:20 +0300

    added OwnerTests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,12 +2,16 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationsPage;
+
+import static java.lang.String.format;
 
 public class TestBase {
 
@@ -15,6 +19,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -23,7 +28,9 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@%s",
+                credentials.login(), credentials.password(), System.getProperty("Url"));
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
